5c216300de53f2ca73cf29e6caeee9ba7c0c53de
stonefury <bfleming@uicalumni.org>
2015-06-16 17:41:19 -0700

    updates

--- kspy.py
+++ kspy.py
@@ -5,15 +5,14 @@ import argparse
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Spy on kafka topics")
-    parser.add_argument('--partition', dest="partition", type=str, required=True, help="partition")
-    parser.add_argument('--offset', dest="offset", type=str, required=False, help="offset to read")
-    parser.add_argument('--topic', dest="topic", type=str, required=True, help="offset to read")
-    parser.add_argument('--consumer', dest="consumer", type=str, required=True, help="offset to read")
-    parser.add_argument('--kafka', dest="kafka", action='store_true', help="offset to read")
+    parser.add_argument('--partition', dest="partition", type=str, required=True, help="Partition to read")
+    parser.add_argument('--offset', dest="offset", type=str, required=False, help="Offset to read")
+    parser.add_argument('--topic', dest="topic", type=str, required=True, help="Kafka topic")
+    parser.add_argument('--consumer', dest="consumer", type=str, required=True, help="Consumer group name")
     # parser.add_argument('--group', dest="group", type=str, required=True, help="offset to read")
-    parser.add_argument('--value', dest="value", type=str, default="", help="not-used")
-    parser.add_argument('--zk', dest="zk", type=str, default="", required=True, help="not-used")
-    parser.add_argument('--broker', dest="broker", type=str, default="", required=True, help="not-used")
+    parser.add_argument('--zk', dest="zk", type=str, default="", required=True, help="Zookeeper:port")
+    parser.add_argument('--broker', dest="broker", type=str, default="", required=True, help="kafkabroker:port")
+    parser.add_argument('--set', dest="set", action="store_true", help="Set the offset to the value you gave in --offset")
 
     args = parser.parse_args()
 
@@ -21,10 +20,12 @@ if __name__ == "__main__":
     zk.start()
 
     data, stats = zk.get('/consumers/{0}/offsets/{1}/{2}'.format(args.consumer, args.topic, args.partition))
-    print "Current value {0}".format(data.decode())
+    old_value = data.decode()
+    print "Current offset {0}".format(data.decode())
 
-    if(args.value):
-        zk.set('/consumers/{0}/offsets/{1}/{2}'.format(args.consumer, args.topic, args.partition), b"%s" % (args.value))
+    if args.offset:
+        zk.set('/consumers/{0}/offsets/{1}/{2}'.format(args.consumer, args.topic, args.partition), b"%s" % (args.offset))
+        print "New offset %s" % (args.offset)
 
     if args.broker:
         kclient = KafkaClient("%s" % (args.broker))
@@ -32,5 +33,15 @@ if __name__ == "__main__":
         # add support for more than 1 parititon
         consumer = SimpleConsumer(kclient, args.consumer, args.topic, partitions=[0])
         consumer.max_buffer_size = None
-        consumer.seek(0, 1)
-        print consumer.get_message()
+        if args.offset:
+            consumer.seek(0, 1)
+
+        message = consumer.get_message()
+        if message:
+            print("MSG: " + str(message[1][3]) + "\tOFFSET: " + str(message[0]) + "\t KEY: " + str(message.message.key) )
+
+    if not args.set:
+        zk.set('/consumers/{0}/offsets/{1}/{2}'.format(args.consumer, args.topic, args.partition), b"%s" % (args.offset))
+        print "Rollback to %s" % (old_value)
+    else:
+        print "Saving offset %s" % (args.offset)
